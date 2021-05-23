cb261f73259899e0c3ec9bd0277953a10689f72f
Agneta <agnetagsu@yahoo.es>
2021-05-23 19:10:45 +0200

    Agregar archivos de proyecto.

---
+++ POO - C2 Ej4/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO___C2_Ej4
+{
+    class Persona
+    {
+        private string nombre;
+
+        public Persona(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public void SetNombre (string elnombre)
+        {
+           this.nombre = elnombre; 
+        }
+
+        public void Saludar()
+        {
+            System.Console.WriteLine("Hola, soy {0}", this.nombre);
+        }
+    }
+}
---
+++ POO - C2 Ej4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace POO___C2_Ej4
+{
+    class PruebaPersona
+    {
+        static void Main(string[] args)
+        {
+            Persona pers1 = new Persona("Cristina");
+            Persona pers2 = new Persona("Carmen");
+
+            pers1.Saludar();
+            pers2.Saludar();
+        }
+    }
+}
